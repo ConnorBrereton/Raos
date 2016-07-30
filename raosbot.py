6926bbe6ed7c13a0e385c96da7905aa5a220a693
ConnorBrereton <breretonconnor@gmail.com>
2016-07-29 22:11:46 -0700

    added: weekday variables, breakfast variables, slight modification to the commands

--- raosbot.py
+++ raosbot.py
@@ -1,6 +1,26 @@
 from itty import *
 import urllib2
 import json
+from bs4 import BeautifulSoup
+
+r = urllib2.urlopen('http://www.aramarkcafe.com/layouts/canary_2015/locationhome.aspx?locationid=4021&pageid=20&stationID=-1')
+soup = BeautifulSoup(r)
+
+#day of the week variables
+day = soup.find_all("div", class_="foodMenuDayColumn")
+monday = day[0].get_header().text
+tuesday = day[1].get_header().text
+wednesday = day[2].get_header().text
+thursday = day[3].get_header().text
+friday = day[4].get_header().text
+
+#breakfast variables
+mbfst = day[0].a["href"].text
+tubfst = day[1].a["href"].text
+wbfst = day[2].a["href"].text
+tbfst = day[3].a["href"].text
+fbfst = day[4].a["href"].text
+
 
 def sendSparkGET(url):
     """
@@ -34,9 +54,12 @@ def index(request):
     When messages come in from the webhook, they are processed here.  The message text needs to be retrieved from Spark,
     using the sendSparkGet() function.  The message text is parsed.  If an expected command is found in the message,
     further actions are taken. i.e.
-    /batman    - replies to the room with text
+    /day    - replies to the room with text
     /batcave   - echoes the incoming text to the room
     /batsignal - replies to the room with an image
+
+    TODO: change the string to represent the commands to represent the new scope. delete the batman image.
+    delete the batman functions in general.
     """
     webhook = json.loads(request.body)
     #print webhook['data']['id']
@@ -46,8 +69,8 @@ def index(request):
     if webhook['data']['personEmail'] != bot_email:
         in_message = result.get('text', '').lower()
         in_message = in_message.replace(bot_name, '')
-        if 'batman' in in_message or "whoareyou" in in_message:
-            msg = "I'm Batman!"
+        if 'day' in in_message:
+            msg = "Okay. Breakfast or Lunch?"
         elif 'batcave' in in_message:
             message = result.get('text').split('batcave')[1].strip(" ")
             if len(message) > 0:
@@ -61,11 +84,6 @@ def index(request):
             print msg
             sendSparkPOST("https://api.ciscospark.com/v1/messages", {"roomId": webhook['data']['roomId'], "text": msg})
     return "true"
-
-#test function. can remove if you'd like
-@get('/hi')
-def hi(request):
-    return 'hi'
 
 #bot email from dev.spark setup process
 bot_email = "raos@sparkbot.io"
