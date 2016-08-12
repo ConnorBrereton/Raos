19157906f4fd6eb8a7c7dedd283cbd67142001fd
ConnorBrereton <breretonconnor@gmail.com>
2016-08-12 14:00:44 -0700

    took out block innards

--- raosbot_single.py
+++ raosbot_single.py
@@ -3,11 +3,10 @@ SJC 11
 """
 
 from itty import *
+from bs4 import BeautifulSoup
 import urllib2
 import json
 import lxml
-from bs4 import BeautifulSoup
-
 
 def soup():
     url1 = 'http://www.aramarkcafe.com/layouts/canary_2015/locationhome.aspx?locationid=4021&pageid=20&stationID=-1'
@@ -106,49 +105,48 @@ def index(request):
     webhook = json.loads(request.body)
     print webhook['data']['id']
     result = sendSparkGET('https://api.ciscospark.com/v1/messages/{0}'.format(webhook['data']['id']))
-    print "test 13"
-    result = json.loads(result)
-    msg = None
-    response = raw_input()
-
-    if webhook['data']['personEmail'] != bot_email:
-        in_message = result.get('text', '').lower()
-        in_message = in_message.replace(bot_name, '')
-
-        #event handler for the building user chooses
-        print "Hello, I'm Raos. I'm here to let you know what food options you have available! What Cisco location are you at? Enter 'idk' if you want me to list the location codes"
-
-        print "Next, tell me what day you want to lookup. Ex: 'monday'"
-
-        if week in in_message:
-            msg = categories[0:4]
-
-        print "Next, tell me what category sounds good. Ex: 'global'"
-        while response in categories[0:4]:
-            if 'breakfast' in in_message:
-                msg = map(meals.__getitem__, (0, 10, 20, 30, 40))
-
-            if 'global' in in_message:
-                msg = map(meals.__getitem__, (11, 31))
-                msg = map(description.__getitem__, (1, 8, 13))
-
-            if 'grill' in in_message:
-                msg = map(meals.__getitem__, (2, 12, 22, 32, 42))
-
-            if 'indian' in in_message:
-                msg = map(meals.__getitem__, (3, 13, 23, 33, 43))
-
-            if 'mediterranean' in in_message:
-                msg = map(meals.__getitem__, (4, 5, 6, 7, 14, 15, 16, 17, 24, 25, 26, 27, 28, 34, 35, 36, 37, 44, 45, 46))
-                msg = map(description.__getitem__, (2, 3, 4, 5, 6, 9, 11))
-            if 'soup' in in_message:
-                msg = map(meals.__getitem__, (8, 9, 18, 19, 28, 29, 38, 39, 47, 48))
-        print "test 14"
-        if msg != None:
-            print msg
-            sendSparkPOST("https://api.ciscospark.com/v1/messages", {"roomId": webhook['data']['roomId'], "text": msg})
+    # print "test 13"
+    # result = json.loads(result)
+    # msg = None
+    #
+    # if webhook['data']['personEmail'] != bot_email:
+    #     in_message = result.get('text', '').lower()
+    #     in_message = in_message.replace(bot_name, '')
+    #
+    #     #event handler for the building user chooses
+    #
+    #     msg = "Hello, I'm Raos. I'm here to let you know what food options you have available! What Cisco location are you at? Enter 'idk' if you want me to list the location codes"
+    #
+    #     msg = "Next, tell me what day you want to lookup. Ex: 'monday'"
+    #
+    #     if week in in_message:
+    #         msg = categories[0:4]
+    #
+    #     print "Next, tell me what category sounds good. Ex: 'global'"
+    #     while response in categories[0:4]:
+    #         if 'breakfast' in in_message:
+    #             msg = map(meals.__getitem__, (0, 10, 20, 30, 40))
+    #
+    #         if 'global' in in_message:
+    #             msg = map(meals.__getitem__, (11, 31))
+    #             msg = map(description.__getitem__, (1, 8, 13))
+    #
+    #         if 'grill' in in_message:
+    #             msg = map(meals.__getitem__, (2, 12, 22, 32, 42))
+    #
+    #         if 'indian' in in_message:
+    #             msg = map(meals.__getitem__, (3, 13, 23, 33, 43))
+    #
+    #         if 'mediterranean' in in_message:
+    #             msg = map(meals.__getitem__, (4, 5, 6, 7, 14, 15, 16, 17, 24, 25, 26, 27, 28, 34, 35, 36, 37, 44, 45, 46))
+    #             msg = map(description.__getitem__, (2, 3, 4, 5, 6, 9, 11))
+    #         if 'soup' in in_message:
+    #             msg = map(meals.__getitem__, (8, 9, 18, 19, 28, 29, 38, 39, 47, 48))
+    #     print "test 14"
+    #     if msg != None:
+    #         print msg
+    sendSparkPOST("https://api.ciscospark.com/v1/messages", {"roomId": webhook['data']['roomId'], "text": msg})
     return "true"
-
 print "test 15"
 #Server configuration. See docs for more details
 
